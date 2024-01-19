13ca7bf1d0edd9200a760fc91412a85bcf9089c8
Bozhidar Andreev <33715265+bojidarandreev@users.noreply.github.com>
2024-01-19 11:02:05 +0100

    Working on the single rental component
    Debugging and remaking some functionality of the SingleRental component.

--- src/pages/SingleRental/SingleRental.jsx
+++ src/pages/SingleRental/SingleRental.jsx
@@ -1,41 +1,58 @@
-import { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 
-import newDataJs from '../../assets/newData.json'
-import Carousel from '../../components/Carousel/Carousel';
+import newDataJs from "../../assets/newData.json";
+import Carousel from "../../components/Carousel/Carousel";
 
-import './singleRental.css'
+import "./singleRental.css";
 
 export default function SingleRental() {
   const [appart, setAppart] = useState(null);
-	const { id } = useParams();
+  const { id } = useParams();
   const navigate = useNavigate();
-  console.log(appart)
+  console.log(appart);
 
   useEffect(() => {
     const data = newDataJs.find((appart) => appart.id === id);
     if (data === undefined) {
-      return navigate('/pas-trouve');
+      return navigate("/pas-trouve");
     }
     setAppart(data);
   }, [id]);
-  if (!appart) return <div>Chargement...</div>
+  if (!appart) return <div>Chargement...</div>;
   return (
     <>
-    <div className="caroselContainer">
+      <div className="caroselContainer">
         <Carousel images={appart.pictures} />
       </div>
       <div className="locationInfoContainer">
         <div className="locationInfo-1">
           <h1 className="locationTitle">{appart.title}</h1>
           <h2 className="locationSubTitle">{appart.location}</h2>
+          <ul className="tagGroup">
+            {appart.tags.map((tag) => (
+              <li>{tag}</li>
+            ))}{" "}
+          </ul>
           {/* <div>{appart.tags.split(',').map((item, i) => <p key={i}>{item}</p>)}</div> */}
         </div>
         <div className="locationInfo-2">
-          <p className="locationRating">Star Rating is {appart.rating} from 5 stars</p>
+          <div>
+            {[...Array(Number(appart.rating))].map((e, i) => {
+              return (
+                <p className="star" key={i}>
+                  {i}
+                </p>
+              );
+            })}
+          </div>
           <div className="logationHost">
-            <p className="locationHostName">{appart.hostName}</p>
-            <img src={appart.hostPicture} className="locationHostImage" alt={appart.hostName}></img>
+            <p className="locationHostName">{appart.host.name}</p>
+            <img
+              src={appart.host.picture}
+              className="locationHostImage"
+              alt={appart.host.name}
+            ></img>
           </div>
         </div>
         <div className="locationInfo-3">
@@ -46,9 +63,9 @@ export default function SingleRental() {
           <div className="locationEquipements">
             <h3 className="locationEquipementsTitle">Equipements</h3>
             {/* <div className="locationEquipementsText">{appart.equipments.split(',').map((item, i) => <p key={i}>{item}</p>)}</div> */}
-          </div> 
+          </div>
         </div>
       </div>
-      </>
+    </>
   );
 }
